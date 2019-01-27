742c71b0c4868cf0e82e6b0ef2d1aa54d22df49c
jolly-hardware-0578 <lmccoy8911@gmail.com>
2019-01-27 00:58:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = ["X", "X", "X", "O", "X", "O", "X", "O", "X"]
+board[0]= "X"
+board[1]="X"
+board[2]= "X"
+board[3]="O"
+board[4]="X"
+board[5]="O"
+board[6]="X"
+board[7]="O"
+board[8]="O"
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
